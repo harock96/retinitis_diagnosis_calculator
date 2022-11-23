4df7d58ca6e892b6369a50e96fe0ebd31246d64a
HaRokJang <61371150+harock96@users.noreply.github.com>
2022-11-23 10:45:00 +0900

    Add files via upload

--- application.py
+++ application.py
@@ -234,4 +234,4 @@ def index():
    prob_total_ARN = prob_total_ARN, prob_total_CMV = prob_total_CMV, prob_total_NIU = prob_total_NIU, prob_max = prob_max)
   
 if __name__ == "__main__":
-  application.run(host = "0.0.0.0", port = "5000", debug=True)
+  application.run(port = "5000", debug=True)
